1acdab6582b45457b2d4e059489f335cf91b1b60
jairofg12 <jairofg12@outlook.com>
2017-11-28 18:35:46 -0500

    Agregado mock de maps

--- Test/spec/s5.google-mapsSpec.js
+++ Test/spec/s5.google-mapsSpec.js
@@ -1,7 +1,12 @@
+/// <reference path="google-maps.mock.js"/>
+/// <reference path="../../s5.js"/>
+/// <reference path="../../s5.google-maps.js"/>
 
-describe('S5 google maps', function(){
-    
-    it('Instance map', function(){
-
+describe('S5 google maps', function(){    
+    describe('Map', function(){
+        it('Instance map', function(){
+            var mapa = new Sinco.utilities.googleMaps('keyprueba');
+            expect(mapa instanceof Sinco.utilities.googleMaps).toBeTruthy();
+        });
     });
 });
--- s5.google-maps.js
+++ s5.google-maps.js
@@ -1,9 +1,21 @@
 (function(Sinco){
-    var mapsGoogle = function(){
-        console.log('Inicializando')
-    }
+    
+    var SincoGoogleMaps = function(key){
+        if (!(this instanceof Sinco.utilities.googleMaps))
+        throw new SincoInitializationError('¡Google maps requiere ser inicializado mediante new Sinco.utilities.googleMaps()!');
 
-    return{
-        mapsGoogle: mapsGoogle
-    }
+        return{
+            init: this.init,
+            demo: function(){
+                console.log('Demo...')
+            }
+        }
+    }  
+
+    SincoGoogleMaps.prototype.init = function(){
+        console.log('Iniciando funcion de maps');
+    }   
+    
+    Sinco.utilities = Sinco.utilities || {};
+    Sinco.utilities.googleMaps = SincoGoogleMaps;
 })(Sinco);
